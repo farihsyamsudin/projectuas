534217a4bda8d7e8d824603dbcd06bd7fe88ad2e
farihsyamsudin <farihsyamsudin31@upi.edu>
2022-12-26 11:06:33 +0700

    debug true lagii

--- projectuas/settings.py
+++ projectuas/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-545x!+@(-&@9vd$+rhmb70=pw=81_63%1dhp+mim!*#g9)of+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", ".railway.app"]
 
@@ -43,12 +43,10 @@ INSTALLED_APPS = [
     'ckeditor',
     'rest_framework',
     'corsheaders',
-    'whitenoise.runserver_nostatic',    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,8 +140,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
