02e907f900db189b2fdbc44477d1c38f904150da
farihsyamsudin <farihsyamsudin31@upi.edu>
2022-12-26 10:45:55 +0700

    debug true lagi

--- projectuas/settings.py
+++ projectuas/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-545x!+@(-&@9vd$+rhmb70=pw=81_63%1dhp+mim!*#g9)of+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", ".railway.app"]
 
