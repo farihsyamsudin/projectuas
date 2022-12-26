3057d46022af2e962c535d5f409bdfbb4e3029a8
farihsyamsudin <farihsyamsudin31@upi.edu>
2022-12-26 10:37:54 +0700

    debug false allowed host

--- projectuas/settings.py
+++ projectuas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-545x!+@(-&@9vd$+rhmb70=pw=81_63%1dhp+mim!*#g9)of+i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["localhost", ".railway.app"]
 
 
 # Application definition
